ee27feb56746f1143ab4918e0128ba440b504bd3
Nishant Aggarwal <121606501+n-aggarwal@users.noreply.github.com>
2023-10-30 14:06:14 -0400

    added documentation and login API endpoint

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -1,9 +1,11 @@
 <?php
 class UserController extends BaseController
 {
-    /*
-    * "/user/list" Endpoint - Get list of users 
-    */
+
+/**
+ * The function is responsible for handling a GET request to retrieve a list of users, with an optional
+ * limit parameter, and returning the list as a JSON response.
+ */
     public function listAction()
     {
         $strErrorDesc = '';
@@ -17,8 +19,6 @@ class UserController extends BaseController
                     $intLimit = $arrQueryStringParams['limit'];
                 }
                 $arrUsers = $userModel->getUsers($intLimit);
-                $arrUsers['query_string'] = $_SERVER['QUERY_STRING'];
-                $arrUsers['error'] = $arrQueryStringParams;
                 $responseData = json_encode($arrUsers);
             } catch (Error $e) {
                 $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
@@ -40,4 +40,74 @@ class UserController extends BaseController
             );
         }
     }
+
+/**
+ * The loginAction function handles the authentication process for a user logging in, including
+ * verifying the username and password, starting a session, and returning a JSON response.
+ */
+    public function loginAction() {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $jsonData = file_get_contents("php://input");
+
+        // Decode the JSON data into a PHP array
+        $data = json_decode($jsonData, true); // Set the second argument to true for an associative array
+
+        // Access the values
+        $username = $data["username"];
+        $password = $data["password"];
+
+        if (strtoupper($requestMethod) == 'POST' && isset($data['username']) && isset($data['password'])) {
+            try {
+                $userModel = new UserModel();
+                $arrUser = $userModel->getUserByUsername($username);
+
+                if (password_verify($password, $arrUser[0]["password"])) {
+                
+                    ini_set('session.cookie_lifetime', 20 * 60);
+                    session_start();
+
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["username"] = $username;
+
+                    $responseData = array(
+                        "success" => true,
+                        "message" => "Authentication successful",
+                        "user_id" => $username,
+                    );
+                    
+                    $responseData = json_encode($responseData);
+
+                }
+                else {
+                    $responseData = array(
+                        "success" => false,
+                        "message" => "Authentication failed",
+                    );
+                    
+                    $responseData = json_encode($responseData);
+                }
+            }
+            catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        }
+        else {
+            $strErrorDesc = 'Method not supported/Wrong Params';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+         // send output 
+         if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+
 }
--- index.php
+++ index.php
@@ -3,18 +3,12 @@
 	error_reporting(E_ALL);
 
 	require "/Applications/XAMPP/xamppfiles/htdocs/inc/bootstrap.php";
-	echo "Hello World";
-
-	echo "Hello World 2\n";
 
 	//CORS Header (NOT SECURE AS OF RIGHT NOW)
 	header ("Access-Control-Allow-Origin:*");
 
 	$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-	echo $uri . "\n";
-		
 	$uri = explode( '/', $uri );
-	echo $uri[2] . "\n";
 
 	if ((isset($uri[2]) && !($uri[2] == 'user' || $uri[2] == 'music')) || !isset($uri[3])) {
 		header("HTTP/1.1 404 Not Found");
@@ -22,9 +16,7 @@
 		exit();
 	}
 
-	echo "Point 1";
 	require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
-	echo "Point 2";
 	$objFeedController = new UserController();
 	$strMethodName = $uri[3] . 'Action';
 	$objFeedController->{$strMethodName}();
